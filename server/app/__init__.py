168448621781e839864324cb310e5cae3578c195
zykis <zykis@rambler.ru>
2017-01-24 15:09:47 +0300

    Deployinh server

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_mail import Mail
+from werkzeug.contrib.fixers import ProxyFix
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.wsgi_app = ProxyFix(app.wsgi_app)
 db = SQLAlchemy(app)
 manager = Manager(app)
 mail = Mail(app)
