8978c89558b051594dfefc5f0333553fb3760630
zykis <zykis@rambler.ru>
2017-03-20 10:24:16 +0300

    Theme update fixed. Still a mess.

--- client/DotaAsker/ThemeSelectionViewController.m
+++ client/DotaAsker/ThemeSelectionViewController.m
@@ -56,44 +56,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)updateRoundSelectedTheme:(Theme*)theme {
+    Round* round = [Round objectForPrimaryKey:@(_roundID)];
+    // Updating round's selected theme
+    LoadingView* loadingView = [[LoadingView alloc] initWithFrame:CGRectMake(self.view.frame.size.width / 2 - 200 / 2, self.view.frame.size.height / 2 - 50 / 2, 200, 50)];
+    [loadingView setMessage:@"Updating round"];
+    [[self view] addSubview:loadingView];
+    
+    RACReplaySubject* subject = [[[ServiceLayer instance] roundService] update:round];
+    [subject subscribeError:^(NSError *error) {
+        [loadingView removeFromSuperview];
+        [self presentAlertControllerWithTitle:@"Round not updated" andMessage:@"Check out connection and try again, please"];
+    } completed:^{
+        // Persist selected theme
+        RLMRealm* realm = [RLMRealm defaultRealm];
+        [realm beginWriteTransaction];
+        [round setSelectedTheme: theme];
+        [realm commitWriteTransaction];
+        [loadingView removeFromSuperview];
+        [self performSegueWithIdentifier:@"themeSelected" sender:theme];
+    }];
+}
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"themeSelected"]) {
         ThemeSelectedViewController *destVC = (ThemeSelectedViewController*)[segue destinationViewController];
-        NSArray* themes = [[[ServiceLayer instance] roundService] themesForRound:[self round]];
-        Theme* selectedTheme;
-        if ([sender isEqual:_imagedButton1]) {
-            selectedTheme = [themes objectAtIndex:0];
-        }
-        else if([sender isEqual:_imagedButton2]) {
-            selectedTheme = [themes objectAtIndex:1];
-        }
-        else if([sender isEqual:_imagedButton3]) {
-            selectedTheme = [themes objectAtIndex:2];
-        }
+//        NSArray* themes = [[[ServiceLayer instance] roundService] themesForRound:[self round]];
+        Theme* selectedTheme = sender;
         
-        Round* round = [Round objectForPrimaryKey:@(_roundID)];
-        // Updating round's selected theme
-        LoadingView* loadingView = [[LoadingView alloc] initWithFrame:CGRectMake(self.view.frame.size.width / 2 - 200 / 2, self.view.frame.size.height / 2 - 50 / 2, 200, 50)];
-        [loadingView setMessage:@"Updating round"];
-        [[self view] addSubview:loadingView];
-        
-        RACReplaySubject* subject = [[[ServiceLayer instance] roundService] update:round];
-        [subject subscribeError:^(NSError *error) {
-            [loadingView removeFromSuperview];
-            [self presentAlertControllerWithTitle:@"Round not updated" andMessage:@"Check out connection and try again, please"];
-        } completed:^{
-            // Persist selected theme
-            RLMRealm* realm = [RLMRealm defaultRealm];
-            [realm beginWriteTransaction];
-            [round setSelectedTheme: selectedTheme];
-            [realm commitWriteTransaction];
-            
-            assert(selectedTheme);
-            [destVC setRoundID:_roundID];
-            [destVC setSelectedThemeID:selectedTheme.ID];
-            
-            [loadingView removeFromSuperview];
-        }];
+        assert(selectedTheme);
+        [destVC setRoundID:_roundID];
+        [destVC setSelectedThemeID:selectedTheme.ID];
     }
 }
 
@@ -101,14 +94,17 @@
 #pragma mark - Navigation
 
 - (IBAction)button1Pressed:(id)sender {
-    [self performSegueWithIdentifier:@"themeSelected" sender:sender];
+    NSArray* themes = [[[ServiceLayer instance] roundService] themesForRound:[self round]];
+    [self updateRoundSelectedTheme:[themes objectAtIndex:0]];
 }
 
 - (IBAction)button2Pressed:(id)sender {
-    [self performSegueWithIdentifier:@"themeSelected" sender:sender];
+    NSArray* themes = [[[ServiceLayer instance] roundService] themesForRound:[self round]];
+    [self updateRoundSelectedTheme:[themes objectAtIndex:1]];
 }
 
 - (IBAction)button3Pressed:(id)sender {
-    [self performSegueWithIdentifier:@"themeSelected" sender:sender];
+    NSArray* themes = [[[ServiceLayer instance] roundService] themesForRound:[self round]];
+    [self updateRoundSelectedTheme:[themes objectAtIndex:2]];
 }
 @end
