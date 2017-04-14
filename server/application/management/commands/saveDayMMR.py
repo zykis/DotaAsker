dad60bcc922d4f204d07546df58e9eb07458ea3f
zykis <zykis@rambler.ru>
2017-04-14 14:36:03 +0300

    fix7

--- server/application/management/commands/saveDayMMR.py
+++ server/application/management/commands/saveDayMMR.py
@@ -1,14 +1,13 @@
 def run():
+    print("STARTED saveDayMMR: {}".format(datetime.now()))
+    t = date.today()
+    strt = t.strftime("%Y-%m-%d")
+    print(strt)
     from application import db
     from application.models import User
     from datetime import date, datetime
 
-    print("STARTED saveDayMMR: {}".format(datetime.now()))
     users_list = User.query.all()
-    t = date.today()
-    strt = t.strftime("%Y-%m-%d")
-    print(strt)
-
     for u in users_list:
         result = db.engine.execute("REPLACE INTO user_date_mmr (user_id, date, mmr) VALUES ({}, '{}', {})".format(u.id, strt, u.mmr))
 
