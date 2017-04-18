ef146c8a4656065fc6c993f7f788dc60c2be5eaf
zykis <zykis@rambler.ru>
2017-04-18 17:54:37 +0300

    Catching exception, when no theme with proper name in DB

--- server/application/parsers/theme_schema.py
+++ server/application/parsers/theme_schema.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields, post_load
 from application.models import Theme
+from sqlalchemy.orm.exc import NoResultFound
 
 class ThemeSchema(Schema):
     id = fields.Int()
@@ -8,5 +9,8 @@ class ThemeSchema(Schema):
     
     @post_load
     def get_theme(self, data):
-        t = Theme.query.filter(Theme.name == data['name']).one()
+        try: 
+            t = Theme.query.filter(Theme.name == data['name']).one()
+        except NoResultFound:
+            print ('Theme with name %s not found in Database' % data['name'])
         return t
