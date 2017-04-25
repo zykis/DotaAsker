cfd3783df0f6c3cf2142ce6ccfc8752b1878d908
zykis <zykis@rambler.ru>
2017-04-25 12:40:58 +0300

    INSERT OR REPLACE

--- server/application/management/commands/saveDayMMR.py
+++ server/application/management/commands/saveDayMMR.py
@@ -9,7 +9,7 @@ def run():
 
     users_list = User.query.all()
     for u in users_list:
-        result = db.engine.execute("REPLACE INTO user_date_mmr (user_id, date, mmr) VALUES ({}, '{}', {})".format(u.id, strt, u.mmr))
+        result = db.engine.execute("INSERT OR REPLACE INTO user_date_mmr (user_id, date, mmr) VALUES ({}, '{}', {})".format(u.id, strt, u.mmr))
 
 if __name__ == '__main__':
     import sys 
