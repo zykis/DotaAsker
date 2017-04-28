8ad656b40343eb8369449eea26eb469c68614c34
zykis <zykis@rambler.ru>
2017-04-28 10:21:12 +0300

    CheckTimeElapsedMatch output edited

--- server/application/management/commands/checkTimeElapsedMatches.py
+++ server/application/management/commands/checkTimeElapsedMatches.py
@@ -6,15 +6,12 @@ def run():
     from application.models import Match, MATCH_FINISHED, MATCH_RUNNING
     from config import MATCH_LIFETIME, MATCH_UPDATELIFE
 
-    print("STARTED checkTimeElapsedMatches: {}".format(datetime.now()))
     match_list = Match.query.all()
-    print('matches: {}'.format(match_list))
     for m in match_list:
         if m.state == MATCH_RUNNING:
             timeDiffInSec = datetime.now() - m.updated_on
-            print('match {} last updated {} hours ago'.format(m.__repr__(), timeDiffInSec.total_seconds() / (60 * 60)))
             if MATCH_UPDATELIFE < timeDiffInSec.total_seconds():
-                print('match {} elapsed'.format(m.__repr__()))
+                print('match {} elapsed. Last updated {} hours ago.'.format(m.__repr__(), timeDiffInSec.total_seconds() / (60 * 60)))
                 # checkout winner
                 m.elapseMatch()
     t = datetime.now()
