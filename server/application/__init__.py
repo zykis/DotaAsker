a67f5fea8c1d887d3a07fdd0b0192e84e175cdd1
zykis <zykis@rambler.ru>
2017-06-16 05:51:35 +0300

    Setting up logger

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from werkzeug.contrib.fixers import ProxyFix
-import logging  
+import logging
 
-logging.basicConfig(fle='dotaasker.log', level=logging.DEBUG)
+# logging.basicConfig(fle='dotaasker.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+handler = logging.handlers.RotatingFileHandler('dotaasker.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 db = SQLAlchemy(app)
